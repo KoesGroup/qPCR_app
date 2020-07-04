c7a451626c827e0c6715131292b1552ff1d1f1a8
tijs bliek <bliek@uva.nl>
2020-07-05 00:06:06 +0200

    typo fix

--- qPCR_app/FetchData.R
+++ qPCR_app/FetchData.R
@@ -30,15 +30,30 @@ GetOutlayers <- function(total, maxDif){
   ## calculates difference between highest and lowest value between
   ## technical replicates, returns a list of the ones exceeding a 
   ## user given value. 
-  lijst2 <- total %>%
+  outLayers <- total %>%
     group_by(Sample,Target) %>%
     mutate("CTmean" = mean(CT), "CTdif"= max(CT)-min(CT)) %>%
     filter(CTdif > maxDif) %>%
     ungroup() %>%
     arrange(CTmean) %>%
     select(File,Sample,Target,CT, CTdif)
+  return(outLayers)
 }
- 
+
+
+TargetValues <- function(total){
+  ## Returns for each target mean, min, 1st quartile, median, 3rd quartile and max CT value.
+  ## take note, in case a sample target combination gives no CT value, it is set to 41.
+  total[is.na(total)] <- 41
+  quartiles <- total %>%
+    group_by(Target) %>%
+    mutate(meanCT = mean(CT), minCT = min(CT), firstCT = quantile(CT, 0.25), medianCT = quantile(CT, 0.5), thirdCT = quantile(CT, 0.75), maxCT = max(CT)) %>%
+    ungroup() %>%
+    select(Target, meanCT, minCT, firstCT, medianCT, thirdCT, maxCT) %>%
+    distinct()
+  return(quartiles)
+}
+
 normalize <- function(total,ref){
   ## normalizes CT values to expression values to a user chosen reference gen.
   refList <- total %>%
@@ -60,16 +75,5 @@ normalize <- function(total,ref){
   return(norm)
 }
 
-TargetValues <- function(total){
-  ## Returns for each target mean, min, 1st quartile, median, 3rd quartile and max CT value.
-  ## take note, in case a sample target combination gives no CT value, it is set to 41.
-  total[is.na(total)] <- 41
-  quartiles <- total %>%
-    group_by(Target) %>%
-    mutate(meanCT = mean(CT), minCT = min(CT), firstCT = quantile(CT, 0.25), medianCT = quantile(CT, 0.5), thirdCT = quantile(CT, 0.75), maxCT = max(CT)) %>%
-    ungroup() %>%
-    select(Target, meanCT, minCT, firstCT, medianCT, thirdCT, maxCT) %>%
-    distinct()
-  return(quartiles)
-}
 
+## geometric average: exp(mean(log(x)))   
