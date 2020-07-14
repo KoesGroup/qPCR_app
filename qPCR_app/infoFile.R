90438fdfb09ca0524eb15f6baa93c7b2201dea04
tijs bliek <bliek@uva.nl>
2020-07-14 09:40:23 +0200

    Added scripts to read and normalize data

---
+++ qPCR_app/infoFile.R
@@ -0,0 +1,28 @@
+uploadInfo <- function(){
+  tekst <- "<h4><font color=\"#CC0000\"><br>This will be containing info on the uploading of the files.</font></h4>"
+  return(tekst)
+}
+
+
+outlayersInfo <- function(){
+  tekst <- "<h4><font color=\"#2EAF2E\"><br>This will be containing info on outlayers of the sample set.</font></h4>"
+  return(tekst)
+}
+
+
+targetsInfo <- function(){
+  tekst <- "<h4><font color=\"#1C1CE3\"><br>This will be containing info on the targets (primerscombinations).</font></h4>"
+  return(tekst)
+}
+
+
+refgeneInfo <- function(){
+  tekst <- "<h4><font color=\"#F5A015\"><br>This will be containing info on what reference gene to use.</font></h4>"
+  return(tekst)
+}
+
+
+refsampleInfo <- function(){
+  tekst <- "<h4><font color=\"#AB2DC4\"><br>This will be containing info using a ref sample or not.</font></h4>"
+  return(tekst)
+}
